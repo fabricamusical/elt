08380b3d7274af781b52e2bbe738a0242f9e5cba
jorgeasmendes <jserpamendes@gmail.com>
2025-07-11 19:16:27 +0000

    Corrigir codigo em disponibilidade_professores

--- dbt/fabricamusical/models/staging/stg_disponibilidade_professores.sql
+++ dbt/fabricamusical/models/staging/stg_disponibilidade_professores.sql
@@ -2,6 +2,6 @@ SELECT
     SAFE.PARSE_DATETIME('%d/%m/%Y %H:%M:%S', created_time) as created_time,
     created_by,
     professor_id,
-    SAFE.PARSE_DATE('%d/%m/%Y', last_update) as dia,
+    SAFE.PARSE_DATE('%d/%m/%Y', dia) as dia,
     horario
 FROM {{ source('planilhas', 'disponibilidade_professores') }}
